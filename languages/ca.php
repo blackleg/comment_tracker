66221d6862aec7fb0ee43cbd5e7114599c82a0b4
blackleg <hectorespertpardo@gmail.com>
2017-05-17 23:21:47 +0200

    Start with catalan translation

---
+++ languages/ca.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Spanish translations for the comment_tracker plugin
+ */
+return array(
+    'comments' => "Comentaris",
+    'comment:notification:settings' => 'Notificacions de comentaris',
+    'comment:notification:settings:description' => 'Avisa\'m quan hi hagi comentaris en elements als que estic subscrit.',
+    'comment:notification:settings:how' => 'Selecciona el mètode de notificació',
+    'comment:notification:settings:linktext' => 'Veure tots els elements als que estic subscrit',
+    'comment:subscriptions' => 'Subscripcions',
+    'comment:subscriptions:none' => 'No hi ha subscripcions actualment',
+    'comment_tracker:setting:notify_owner' => "¿Permitir que el rastreador maneje las notificaciones de los propietarios de los comentarios?",
+    'comment:subscribe:tooltip' => 'Subscriu-te per rebre notificacions quan hi hagi comentaris',
+    'allow:comment:notification' => 'Vols activar les notificacions? ',
+    'email:content:type' => 'Vols rebre els correus en format HTML? ',
+    'text:email' => 'No',
+    'html:email' => 'Sí',
+    'comment:subscribe' => 'Subscriure\'m',
+    'comment:unsubscribe' => 'Cancel·lar la subscripció',
+    'comment:subscribe:long' => 'Subscriure\'m a les notificacions de comentaris',
+    'comment:unsubscribe:long' => 'Cancel·lar la subscripció a les notificacions de comentaris',
+    'comment_tracker:setting:autosubscribe' => "Auto-subscriure'm als continguts on hagi fet comentaris?",
+    'show:subscribers' => 'Mostra subscriptors',
+    'comment:subscribe:success' => 'T\'has subscrit correctament a aquest contingut o temàtica.',
+    'comment:subscribe:failed' => "Ho sentim! No hem pogut subscriure't correctament a aquest contingut o temàtica.",
+    'comment:subscribe:entity:not:access' => "Ho sentim! No hem pogut trobar aquest contingut o temàtica.",
+    'comment:unsubscribe:success' => 'S\'ha cancel·lat correctament la teva subscripció a aquest contingut o temàtica.',
+    'comment:unsubscribe:failed' => "Ho sentim! No has pogut cancel·lar la subscripció a aquest contingut o temàtica.",
+    'comment:unsubscribe:not:valid:url' => 'Ho sentim! Aquesta url no és vàlida per cancel·lar la subscripció a aquest contingut o temàtica.',
+    'comment:unsubscribe:entity:not:access' => "Ho sentim! No hem trobat el contingut o temàtica.",
+    'comment_tracker:setting:show_button' => "Mostrar el botó de subscripció/cance·lació a sobre dels comentaris?",
+    'comment_tracker:setting:show_entity_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los componentes?",
+    'comment_tracker:setting:show_river_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los elementos del River?",
+    'comment_tracker:item' => "element",
+    'comment_tracker:notify:subject' => '%s ha comentat en el %s "%s"',
+    'comment_tracker:notify:subject:group' => '%s ha comentat en el %s "%s" en el grup %s',
+    'comment_tracker:notify:body' => "Hola %s
+
+Hi ha un nou comentari a \"%s\".
+
+%s ha escrit:
+\"%s\"
+
+Puedes responder a la entrada aquí:
+%s
+
+
+--
+Has recibido esta notificación porque te has suscrito para recibir notificaciones sobre nuevos comentarios. Para cambiar las preferencias de notificación de este tipo de mensajes, haz clic aquí:
+%s
+",
+);
