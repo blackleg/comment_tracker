f9b066586a25c5bf667ec7aee3e4bc92f35d4c34
blackleg <hectorespertpardo@gmail.com>
2017-05-18 22:44:31 +0200

    Start galician

--- languages/gl.php
+++ languages/gl.php
@@ -1,41 +1,41 @@
 <?php
 
 /**
- * Spanish translations for the comment_tracker plugin
+ * Galician translations for the comment_tracker plugin
  */
 return array(
     'comments' => "Comentarios",
-    'comment:notification:settings' => 'Notificaciones de comentarios',
-    'comment:notification:settings:description' => 'Notificarme cuando haya comentarios en elementos en los que te hayas suscrito.',
-    'comment:notification:settings:how' => 'Selecciona tu método de notificación',
-    'comment:notification:settings:linktext' => 'Ver todos los elementos en los que estoy suscrito.',
-    'comment:subscriptions' => 'Suscripciones',
-    'comment:subscriptions:none' => 'No hay suscripciones actualmente',
+    'comment:notification:settings' => 'Notificacións de comentarios',
+    'comment:notification:settings:description' => 'Notificarme cando haxa comentarios en elementos nos que me subscribín',
+    'comment:notification:settings:how' => 'Selecciona o teu método de notificación',
+    'comment:notification:settings:linktext' => 'Ver todos os elementos nos que me subscribín',
+    'comment:subscriptions' => 'Subscricións',
+    'comment:subscriptions:none' => 'Actualmente non hai subscricións',
     'comment_tracker:setting:notify_owner' => "¿Permitir que el rastreador maneje las notificaciones de los propietarios de los comentarios?",
-    'comment:subscribe:tooltip' => 'Suscríbete para recibir notificaciones cuando haya comentarios',
-    'allow:comment:notification' => '¿Quieres habilitar las notificaciones? ',
-    'email:content:type' => '¿Deseas recibir correos en formato HTML? ',
-    'text:email' => 'No',
+    'comment:subscribe:tooltip' => 'Subscríbete para recibir notificacións cando haxa comentarios sobre este contido',
+    'allow:comment:notification' => 'Queres habilitar as notificacións? ',
+    'email:content:type' => 'Desexas recibir correos en formato HTML? ',
+    'text:email' => 'Non',
     'html:email' => 'Si',
     'comment:subscribe' => 'Subscribirme',
-    'comment:unsubscribe' => 'Cancelar suscripción',
-    'comment:subscribe:long' => 'Suscribirme a las notificaciones de comentarios',
-    'comment:unsubscribe:long' => 'Cancelar suscripción de las notificaciones de comentarios',
-    'comment_tracker:setting:autosubscribe' => "¿Auto-suscribirme al contenido en el que haya comentado?",
-    'show:subscribers' => 'Mostrar suscriptores',
-    'comment:subscribe:success' => 'Te has suscrito correctamente a este contenido o temática.',
-    'comment:subscribe:failed' => "Lo sentimos! No hemos podido suscribirte a este contenido o temática.",
-    'comment:subscribe:entity:not:access' => "Lo sentimos! No hemos encontrado el contenido o temática por alguna razón.",
-    'comment:unsubscribe:success' => 'Se ha cancelado correctamente la suscripción a este contenido o temática.',
-    'comment:unsubscribe:failed' => "Lo sentimos! No hemos podido cancelar la suscripción a este contenido o temática.",
-    'comment:unsubscribe:not:valid:url' => 'Lo sentimos! Esta no es una url correcta para cancelar la suscipción de este contenido o temática.',
-    'comment:unsubscribe:entity:not:access' => "Lo sentimos! No hemos encontrado el contenido o temática.",
-    'comment_tracker:setting:show_button' => "¿Mostrar el botón de suscripción/cancelación arriba del comentario?",
+    'comment:unsubscribe' => 'Cancelar subscrición',
+    'comment:subscribe:long' => 'Subscribirme ás notificacións de comentarios',
+    'comment:unsubscribe:long' => 'Cancelar subscrición das notificacións de comentarios',
+    'comment_tracker:setting:autosubscribe' => "Auto-subscribirme ao contido no que haxa comentarios?",
+    'show:subscribers' => 'Amosar subscritores',
+    'comment:subscribe:success' => 'Subscribícheste correctamente a este contido ou temática.',
+    'comment:subscribe:failed' => "Sentímolo! Non podes subscribirte a este contido ou temática.",
+    'comment:subscribe:entity:not:access' => "Sentímolo! Por algunha razón non puidemos atopar o contido ou temática.",
+    'comment:unsubscribe:success' => 'Cancelaches correctamente a túa subscrición a este contido ou temática.',
+    'comment:unsubscribe:failed' => "Sentímolo! Non puideches cancelar a túa subscrición a este contido ou temática.",
+    'comment:unsubscribe:not:valid:url' => 'Sentímolo! Esta non é unha url válida para cancelar a subscrición a este contido ou temática.',
+    'comment:unsubscribe:entity:not:access' => "Sentímolo! Non puidemos atopar o contido ou temática.",
+    'comment_tracker:setting:show_button' => "Amosar o botón de subscrición/cancelación por riba da vista de comentarios?",
     'comment_tracker:setting:show_entity_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los componentes?",
     'comment_tracker:setting:show_river_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los elementos del River?",
     'comment_tracker:item' => "elemento",
-    'comment_tracker:notify:subject' => '%s comentó en el %s "%s"',
-    'comment_tracker:notify:subject:group' => '%s comentó en el %s "%s" en el grupo %s',
+    'comment_tracker:notify:subject' => '%s comentou no %s "%s"',
+    'comment_tracker:notify:subject:group' => '%s comentou no %s "%s" no grupo %s',
     'comment_tracker:notify:body' => "Hola %s
 
 Hay un nuevo comentario en \"%s\".
@@ -43,7 +43,7 @@ Hay un nuevo comentario en \"%s\".
 %s escribió:
 \"%s\"
 
-You can reply to the post here:
+Puedes responder a la entrada aquí:
 %s
 
 
